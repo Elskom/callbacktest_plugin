47c58710f3f655dd16883b8eb9eaa65cbedb21d5
CatGirlsAreLife <20854328+CatGirlsAreLife@users.noreply.github.com>
2018-08-31 17:35:04 -0400

    Change clone branch to the feature branch.

--- update-externals.ps1
+++ update-externals.ps1
@@ -2,7 +2,7 @@ Set-Location -Path externals
 $env:elskomnewpth = Join-Path (Get-Location) Els_kom_new
 if(!(Test-Path -Path $env:elskomnewpth))
 {
-    git clone -q https://github.com/Elskom/Els_kom_new.git --recursive
+    git clone -q https://github.com/Elskom/Els_kom_new.git --branch add-plugins-installer-form --recursive
     Set-Location -Path Els_kom_new
     msbuild PCbuild/pcbuild.sln /nologo /verbosity:m /m
     Set-Location -Path ..
