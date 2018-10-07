a686339c269fe7f328be2fe9c5db11798b84b3f7
CatGirlsAreLife <20854328+CatGirlsAreLife@users.noreply.github.com>
2018-10-07 19:50:00 -0400

    Fixed nuget restore.

--- update-externals.ps1
+++ update-externals.ps1
@@ -3,17 +3,17 @@ $env:elskomnewpth = Join-Path (Get-Location) Els_kom_new
 if(!(Test-Path -Path $env:elskomnewpth))
 {
     git clone -q https://github.com/Elskom/Els_kom_new.git --recursive
-    Set-Location -Path Els_kom_new
+    Set-Location -Path Els_kom_new/PCbuild
     nuget restore
-    msbuild PCbuild/pcbuild.sln /nologo /verbosity:m /m
-    Set-Location -Path ..
+    msbuild pcbuild.sln /nologo /verbosity:m /m
+    Set-Location -Path ../..
 }
 else
 {
-    Set-Location -Path Els_kom_new
+    Set-Location -Path Els_kom_new/PCbuild
     git pull -q
     nuget restore
-    msbuild PCbuild/pcbuild.sln /nologo /verbosity:m /m
-    Set-Location -Path ..
+    msbuild pcbuild.sln /nologo /verbosity:m /m
+    Set-Location -Path ../..
 }
 Set-Location -Path ..
